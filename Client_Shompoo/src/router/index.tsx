428f416dc0af2fd60b88e44daacb61984f6e5362
Quang-Huy-coder <huynqph51298@gmail.com>
2025-05-17 22:28:57 +0700

    connectAPI

--- Client_Shompoo/src/router/index.tsx
+++ Client_Shompoo/src/router/index.tsx
@@ -1,8 +1,8 @@
 import MainLayout from "components/layout/MainLayout";
 import About from "pages/About/About";
 import Home from "pages/Home";
-import React from "react";
-import { createBrowserRouter, Link } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
+
 
 export const router = createBrowserRouter([
   {
