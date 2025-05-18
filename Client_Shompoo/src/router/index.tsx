8fbbce5f72b2056f2201b099a8d1e1ed48dc96c7
Quang-Huy-coder <huynqph51298@gmail.com>
2025-05-18 10:01:09 +0700

    giaodienRegister

--- Client_Shompoo/src/router/index.tsx
+++ Client_Shompoo/src/router/index.tsx
@@ -1,6 +1,7 @@
 import MainLayout from "components/layout/MainLayout";
 import About from "pages/About/About";
 import Home from "pages/Home";
+import Register from "pages/Register/Register";
 import { createBrowserRouter } from "react-router-dom";
 
 
@@ -17,6 +18,10 @@ export const router = createBrowserRouter([
         path: "about",     
         element: <About />,
       },
+      {
+        path: "register",     
+        element: <Register />,
+      }
     ],
   },
 ]);
